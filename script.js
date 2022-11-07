f55957151c9141a9dbf88a525d03057f96f5ed5b
Aislin <72391548+Xuanluo-Qiu@users.noreply.github.com>
2022-11-07 14:51:09 +0800

    #5 使用vue接入后端，优化页面，增加文章页面

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-// Function handles active menu item    
-$(function () {
-    $('.nav-btn').on('click', function () {
-        $(this).parent().find('.active').removeClass('active');
-        $(this).addClass('active');
-    });
-});
+var api = "https://v2.api.light.xhhzs.cn/v2"
+
+var 公告 = new Vue({
+    el: '#公告',
+    data() {
+        return {
+            message: null
+        }
+    },
+    mounted() {
+        axios
+            .get(api + '/announcement')
+            .then(response => (this.message = response.data.data))
+            .catch(function (error) { // 请求失败处理
+                console.log(error);
+            })
+    }
+})
+
+var 文章列表_time = new Vue({
+    el: '#文章列表-time',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        axios
+            .get(api + '/post?order=2')
+            .then(response => (this.posts = response.data.data))
+            .catch(function (error) { // 请求失败处理
+                console.log(error);
+            })
+    }
+})
